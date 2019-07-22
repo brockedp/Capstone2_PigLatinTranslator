ff047609be9a57882a09fd2a5c3ac6b708d84481
Deno <denobrockert@yahoo.com>
2019-07-22 14:09:44 -0400

    Pig Latin Translator push

---
+++ Capstone2_PigLatin/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Capstone2_PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            bool translateAgain = true;
+            do
+            {
+                string userString = GetInput("Please enter some text to be translated: ");
+                TranslatePigLatin(userString);
+                Console.WriteLine();
+                string againInput = "";
+                while(againInput != "y" && againInput != "n")
+                {
+                    againInput = GetInput("Would you like to play again? y/n: ");
+                }
+                if (againInput == "n")
+                {
+                    translateAgain = false;
+                }
+
+
+
+
+            } while (translateAgain);
+            Console.WriteLine("Oodgay Yebay!");
+
+        }
+
+        public static string GetInput(string message)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine().ToLower();
+            if (input == "")
+            {
+                return GetInput("That is not an input. Please try again: ");
+            }
+            else
+            {
+                return input;
+            }
+        }
+
+        public static void TranslatePigLatin(string message)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+            string vowelFirst = "way";
+            string consonantFirst = "ay";
+            string[] inputArray = message.Split(" ");
+            int length = inputArray.Length;
+           
+
+            foreach (string word in inputArray)
+            {
+                 
+               int indexNum = word.IndexOfAny(vowels);
+               if (indexNum == 0)
+               {
+                   string vowelPigLatin = word + vowelFirst;
+                   Console.Write(vowelPigLatin + " ");
+                   
+               
+               }
+               else if (indexNum > 0)
+               {
+                   string substring1 = word.Substring(0, indexNum);
+                   string substring2 = word.Substring(indexNum);
+                   string consonantPigLatin = substring2 + substring1 + consonantFirst;
+                   Console.Write(consonantPigLatin + " ");
+               }
+               else
+               {
+                   Console.Write(word +" ");
+               }
+
+               
+
+            }
+
+        }
+       
+    }
+}
